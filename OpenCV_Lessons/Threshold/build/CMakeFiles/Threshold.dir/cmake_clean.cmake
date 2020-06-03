2bbcd27428cb9fc358432e57929cb813193d3d66
enes doruk <42621368+enesdoruk@users.noreply.github.com>
2020-06-03 05:10:55 +0300

    Add files via upload

---
+++ OpenCV_Lessons/Threshold/build/CMakeFiles/Threshold.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Threshold.dir/opencv.cpp.o"
+  "Threshold.pdb"
+  "Threshold"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Threshold.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
