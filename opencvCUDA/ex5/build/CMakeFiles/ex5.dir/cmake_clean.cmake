c3d724694327ef454ad00e688986471cb7302eef
enes doruk <42621368+enesdoruk@users.noreply.github.com>
2020-10-15 11:48:45 +0300

    Add files via upload

---
+++ opencvCUDA/ex5/build/CMakeFiles/ex5.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ex5.dir/ex5.cpp.o"
+  "ex5.pdb"
+  "ex5"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ex5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
