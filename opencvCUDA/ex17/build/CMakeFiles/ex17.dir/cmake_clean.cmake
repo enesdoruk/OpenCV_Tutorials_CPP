92be45a9027ed8da9b763eecf08aac83b1cd4a15
enes doruk <42621368+enesdoruk@users.noreply.github.com>
2020-10-15 12:06:05 +0300

    Add files via upload

---
+++ opencvCUDA/ex17/build/CMakeFiles/ex17.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ex17.dir/ex17.cpp.o"
+  "ex17.pdb"
+  "ex17"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ex17.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ opencvCUDA/ex18/build/CMakeFiles/ex18.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ex18.dir/ex18.cpp.o"
+  "ex18.pdb"
+  "ex18"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ex18.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
